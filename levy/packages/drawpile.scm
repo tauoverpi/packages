dcedaa7fe62dc7138b3a03633d44f22a911abf20
Simon <simonank@mail.ru>
2019-06-06 09:11:45 +0200

    add drawpile

---
+++ levy/packages/drawpile.scm
@@ -0,0 +1,34 @@
+(define-module (levy packages drawpile)
+ #:use-module (gnu packages qt)
+ #:use-module (guix packages)
+ #:use-module (guix download)
+ #:use-module (guix build-system cmake)
+ #:use-module (gnu packages kde-frameworks))
+
+(define-public drawpile
+  (package
+    (name "drawpile")
+    (version "2.1.10")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://github.com/drawpile/Drawpile/archive/"
+                                  version
+                                  ".tar.gz"))
+              (sha256
+                (base32
+                  "0m3pri9kfz0gplgslcz4sn36khs03g1457xppczw4s87b168gkg7"))))
+    (build-system cmake-build-system)
+    (arguments
+      '(#:tests? #f))
+    (native-inputs
+      `(("extra-cmake-modules" ,extra-cmake-modules)))
+    (inputs
+      `(("qtbase" ,qtbase)
+        ("qtmultimedia" ,qtmultimedia)
+        ("qtsvg" ,qtsvg)
+        ("karchive" ,karchive)))
+    (home-page "")
+    (description "")
+    (synopsis "")
+    (license #f)))
+
