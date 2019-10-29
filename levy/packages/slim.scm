d2a1d23cfaf8da98f5a6f56bf0d5e53d4833ff23
Simon <tauoverpi@yandex.com>
2019-10-29 22:41:37 +0100

    add slim themes

---
+++ levy/packages/slim.scm
@@ -0,0 +1,32 @@
+(define-module (levy packages slim)
+  #:use-module (guix packages)
+  #:use-module (guix build-system trivial)
+  #:use-module (guix git-download))
+
+(define-public slim-themes
+  (let ((commit "f2893415211fbc02477b9d46f37e3118a8c11982")
+        (revision "1"))
+    (package
+      (name "slim-themes")
+      (version (string-append version "-" revision "." (string-take commit 7)))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                       (url "https://github.com/adi1090x/slim_themes")
+                       (commit commit)))
+                (sha256
+                  (base32
+                    "1dinb0afg6cyv5m3pgs0yamqsbgi986w0m7w89jwkw09qnf7hvll"))))
+      (build-system trivial-build-system)
+      (arguments
+        '(#:modules ((guix build utils))
+          #:builder
+          (let ((out (assoc-ref %outputs "out"))
+                (src (assoc-ref %build-inputs "source")))
+            (begin (use-modules (guix build utils))
+                   (mkdir out)
+                   (copy-recursively (string-append src "/themes") out)))))
+      (synopsis "")
+      (description "")
+      (home-page "https://github.com/adi1090x/slim_themes")
+      (license #f))))
