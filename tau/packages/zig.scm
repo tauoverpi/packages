04847bfb8cdab6e002f861378a574390d96a73ff
Simon A. Nielsen Knights <tauoverpi@yandex.com>
2020-06-04 19:21:22 +0200

    Add zig-0.6.0

--- tau/packages/zig.scm
+++ tau/packages/zig.scm
@@ -41,6 +41,59 @@
     (home-page "https://ziglang.org")
     (license license:expat)))
 
+(define-public lld-10
+  (package
+    (name "lld")
+    (version "10.0.0")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                     "https://github.com/llvm/llvm-project/releases/download/llvmorg-"
+                     version "/lld-" version".src.tar.xz"))
+              (sha256
+                (base32
+                  "026pwcbczcg0j5c9h7hxxrn3ki81ia9m9sfn0sy0bvzffv2xg85r"))))
+    (build-system cmake-build-system)
+    (arguments '(#:tests? #f))
+    (inputs `(("llvm" ,llvm-10)))
+    (description "")
+    (synopsis description)
+    (home-page "")
+    (license license:expat)))
+
+;https://ziglang.org/builds/zig-0.6.0+6524a64bd.tar.xz
+(define-public zig-0.6.0
+  (package
+    (version "0.6.0+6524a64bd")
+    (name "zig-0.6.0")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://ziglang.org/builds/zig-"
+                                  version ".tar.xz"))
+              (sha256
+                (base32
+                  "1g495wkqr2a3rzccpv6ghrwrc16yzklkdyrpfbn35f6vw8rp1xv1"))))
+    (build-system cmake-build-system)
+    (arguments
+     `(#:tests? #f
+       #:phases
+       (modify-phases %standard-phases
+         (add-before 'build 'fix-build
+           (lambda _
+              (setenv "HOME" "/tmp")
+              #t)))))
+    (inputs
+      `(("llvm" ,llvm-10)
+        ("clang" ,clang-10)
+        ("lld" ,lld-10)
+        ("libxml2" ,libxml2)
+        ("zlib" ,zlib)))
+    (description
+      "Programming languaged designed for robustness, optimality, and clarity")
+    (synopsis description)
+    (home-page "https://ziglang.org")
+    (license license:expat)))
+
 
 (define-public zig-0.5.0+28a6c136e
   (package/inherit zig-0.5.0
